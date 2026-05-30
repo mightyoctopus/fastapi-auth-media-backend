e5eab225f2d3726a25b6aad19904fe4c43f02f67
mhhong <mh.hong@proton.me>
2026-05-30 21:25:08 +0900

    Implement ImageKit SDK and build image uploading logic in upload_file endpoint

--- app/app.py
+++ app/app.py
@@ -5,6 +5,13 @@ from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+from app.images import imagekit
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+import shutil
+import os
+from datetime import datetime
+
 
 
 
@@ -19,29 +26,64 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-# 1. Request with upload file
-# 2. Take the file and create a temp file
-# 3. Access the database and upload the temp file
-# 4. Remove the temp file after all process is complete
 @app.post("/upload")
 async def upload_file(
         file: UploadFile = File(...),
         caption: str = Form(...),
         session: AsyncSession = Depends(get_async_session)
 ):
+    # File extension type/name
+    file_suffix = Path(file.filename).suffix
 
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy name"
-    )
+    timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    # File name concatenated with datetime
+    file_name = f"{timestamp}-{str(Path(file.filename))}"
 
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
+    temp_file_path = None
 
-    return post
+    try:
+        with NamedTemporaryFile(delete=False, suffix=file_suffix) as tmp_file:
+
+            shutil.copyfileobj(file.file, tmp_file)
+            temp_file_path = tmp_file.name
+            print(f"File temporarily stored at: {temp_file_path}")
+
+
+        if not temp_file_path or not os.path.exists(temp_file_path):
+            raise HTTPException(
+                status_code=404,
+                detail="File not found"
+            )
+
+        response = imagekit.files.upload(
+            file=Path(temp_file_path),
+            file_name=file_name,
+            folder="/uploads",
+        )
+
+        post = Post(
+            caption=caption,
+            url=response.url,
+            file_type="video" if file.content_type.startswith("video") else "photo",
+            file_name=file_name
+        )
+
+        session.add(post)
+        await session.commit()
+        await session.refresh(post)
+
+        return {
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+        }
+    finally:
+        # Clean up temp file
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
 
 
 @app.get("/feed")
