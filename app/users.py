fe05347b1e4bfe0de269ec709e548e3ed32ae496
mhhong <mh.hong@proton.me>
2026-06-04 03:41:49 +0900

    Build UserManager

--- app/users.py
+++ app/users.py
@@ -9,3 +9,53 @@ from fastapi_users.authentication import(
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
+
+
+SECRET = "fta3erfdva2f4ea4b5c6d7e6f7a2f4c4d5db3213defgrg4h8b9r38f9"
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    #===== Hooks =====#
+    # When user registered
+    async def on_after_register(
+        self, user: User, request: Optional[Request] = None
+    ) -> None:
+        print(f"User {user.id} has registered.")
+
+    # When password reset requested
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ) -> None:
+        print(f"User {user.id} has forgot the password. Reset token: {token}")
+
+    # Email verification requested
+    async def on_after_request_verify(
+        self, user: User, token:str, request: Optional[Request] = None
+    ) -> None:
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+
+async def get_user_manager(
+        user_db: SQLAlchemyUserDatabase = Depends(get_user_db)
+):
+    yield UserManager(user_db) # user_db fed for BaseUserManager (__init__)
+
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+def get_jwt_strategy():
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
+current_active_user = fastapi_users.current_user(active=True)
