bae697fd981a0833d8e0d0544e0193b85f83609d
mhhong <mh.hong@proton.me>
2026-06-03 22:04:31 +0900

    Connect User class with FastAPI user auth system

--- app/db.py
+++ app/db.py
@@ -1,11 +1,14 @@
 from collections.abc import AsyncGenerator
 import uuid
 
+from fastapi.params import Depends
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+
 
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
@@ -15,6 +18,10 @@ class Base(DeclarativeBase):
     pass
 
 
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship("Post", back_populates="user")
+
+
 class Post(Base):
     """
     ORM database model for posts
@@ -22,11 +29,14 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="posts")
 
 
 engine = create_async_engine(DATABASE_URL)
@@ -42,3 +52,11 @@ async def create_db_and_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+
+# The SQLAlchemyUserDatabase acts as a bridge between:
+# class User and the FastAPI Users authentication system
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    # get user database table
+    yield SQLAlchemyUserDatabase(session, User ) # Class Instantiation
