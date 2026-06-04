406e899b21b67e609c387441053f206dc48c4434
mhhong <mh.hong@proton.me>
2026-06-05 03:03:17 +0900

    Add User Manager Schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
+from fastapi_users import schemas
+import uuid
+
+### Defines the shape of data moving through the API. ###
 
 class CreatePost(BaseModel):
     title: str
@@ -7,3 +11,15 @@ class CreatePost(BaseModel):
 class PostResponse(BaseModel):
     title: str
     content: str
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
+
+
