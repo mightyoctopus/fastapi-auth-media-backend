8faa022049bd3c2d2b2b38e5d860fd531b186a88
mhhong <mh.hong@proton.me>
2026-06-05 03:02:28 +0900

    Implement UserManager Routers

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
 import uuid
 from typing import Optional
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
-from app.schemas import CreatePost, PostResponse
+from app.schemas import CreatePost, PostResponse, UserCreate, UserUpdate, UserRead
 from app.db import Post, create_db_and_tables, get_async_session
+from app.images import imagekit
+from app.users import auth_backend, current_active_user, fastapi_users
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
-from app.images import imagekit
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 import shutil
@@ -25,6 +26,12 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
 
 
 @app.post("/upload")
